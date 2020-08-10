42e2bde7e65286bbb77a5971b7bdd502fae31de2
TranNhutTruong <Trantruong7026@gmail.com>
2020-08-10 08:47:01 +0700

    TranNhutTruong
    1

--- TranNhutTruong/2/2-content.php
+++ TranNhutTruong/2/2-content.php
@@ -16,126 +16,126 @@ $url_path = str_replace('\\', '/', $url_path);
 
 <div class="type-2">
 
-<!-- Header -->
-<header id="top" class="header-home">
-  <div class="brand-panel">
-    <a class="brand js-target-scroll" href="./">
-      go<span class="text-primary">.</span>arch </a>
-    <div class="brand-name">Go.arch</div>
-  </div>
-  <div class="header-phone">+9 (875) 456-09-28</div>
-  <div class="vertical-panel"></div>
-  <div class="vertical-panel-content">
-
-
-  </div>
-
-  <!-- Navigation Desctop -->
-  <nav class="navbar-desctop visible-md visible-lg">
-    <div class="container">
+  <!-- Header -->
+  <header id="top" class="header-home">
+    <div class="brand-panel">
       <a class="brand js-target-scroll" href="./">
         go<span class="text-primary">.</span>arch </a>
-      <div class="navbar-desctop-menu">
-
-        <ul class="menu">
-          <li class="item-101  deeper dropdown parent"><a class="dropdown-toggle" href="./homepage">Home <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-108"><a href="./homepage/home-light">Home - Light</a></li>
-              <li class="item-109 active"><a href="./">Home - Dark</a></li>
-            </ul>
-          </li>
-          <li class="item-110"><a href="./about-us">About Us</a></li>
-          <li class="item-111 deeper dropdown parent"> <a class="btn-scroll" href="#">Projects <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-112"><a href="./projects/projects-list">Projects - List</a></li>
-              <li class="item-113"><a href="./projects/projects-details">Projects - Details</a></li>
-            </ul>
-          </li>
-          <li class="item-114 deeper dropdown parent"><a class="dropdown-toggle" href="./blog">Blog <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-115"><a href="./blog/blog-list">Blog - List</a></li>
-              <li class="item-116"><a href="./blog/blog-post">Blog - Post</a></li>
-            </ul>
-          </li>
-          <li class="item-117"><a href="./contacts">Contacts</a></li>
-        </ul>
-      </div>
+      <div class="brand-name">Go.arch</div>
     </div>
-  </nav>
-
-  <!-- Navigation Mobile -->
-  <nav class="navbar-mobile">
-    <a class="brand js-target-scroll" href="/2/">
-      go<span class="text-primary">.</span>arch </a>
+    <div class="header-phone">+9 (875) 456-09-28</div>
+    <div class="vertical-panel"></div>
+    <div class="vertical-panel-content">
 
 
-    <!-- Navbar Collapse -->
-    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-mobile">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbar-mobile">
-      <div class="navbar-nav-mobile">
-
-        <ul class="menu">
-          <li class="item-101  deeper dropdown parent"><a class="dropdown-toggle" href="./homepage">Home <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-108"><a href="./homepage/home-light">Home - Light</a></li>
-              <li class="item-109 active"><a href="./">Home - Dark</a></li>
-            </ul>
-          </li>
-          <li class="item-110"><a href="./about-us">About Us</a></li>
-          <li class="item-111 deeper dropdown parent"> <a class="btn-scroll" href="#">Projects <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-112"><a href="./projects/projects-list">Projects - List</a></li>
-              <li class="item-113"><a href="./projects/projects-details">Projects - Details</a></li>
-            </ul>
-          </li>
-          <li class="item-114 deeper dropdown parent"><a class="dropdown-toggle" href="./blog">Blog <i class="fa fa-angle-down"></i></a>
-            <ul>
-              <li class="item-115"><a href="./blog/blog-list">Blog - List</a></li>
-              <li class="item-116"><a href="./blog/blog-post">Blog - Post</a></li>
-            </ul>
-          </li>
-          <li class="item-117"><a href="./contacts">Contacts</a></li>
-        </ul>
-      </div>
     </div>
-  </nav>
-</header>
 
-<div class="layout">
+    <!-- Navigation Desctop -->
+    <nav class="navbar-desctop visible-md visible-lg">
+      <div class="container">
+        <a class="brand js-target-scroll" href="./">
+          go<span class="text-primary">.</span>arch </a>
+        <div class="navbar-desctop-menu">
 
-  <!-- Start fullwidth-1 -->
-
-
-  <main class="main">
-
-
-    <!-- Start revolution slider -->
-
-    <div class="rev_slider_wrapper">
-      <div id="rev_slider" class="rev_slider fullscreenbanner">
-        <ul>
-          <!-- Slide 1 -->
-          <li data-transition="slotzoom-horizontal" data-slotamount="7" data-masterspeed="1000" data-fsmasterspeed="1000">
-
-            <!-- Main image-->
-
-            <img src="./images/slide1.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg">
-
-
-
-
-
-
-        </ul>
+          <ul class="menu">
+            <li class="item-101  deeper dropdown parent"><a class="dropdown-toggle" href="./homepage">Home <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-108"><a href="#">Home - Light</a></li>
+                <li class="item-109 active"><a href="./">Home - Dark</a></li>
+              </ul>
+            </li>
+            <li class="item-110"><a href="./about-us">About Us</a></li>
+            <li class="item-111 deeper dropdown parent"> <a class="btn-scroll" href="#">Projects <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-112"><a href="#">Projects - List</a></li>
+                <li class="item-113"><a href="#">Projects - Details</a></li>
+              </ul>
+            </li>
+            <li class="item-114 deeper dropdown parent"><a class="dropdown-toggle" href="./blog">Blog <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-115"><a href="#">Blog - List</a></li>
+                <li class="item-116"><a href="#">Blog - Post</a></li>
+              </ul>
+            </li>
+            <li class="item-117"><a href="#">Contacts</a></li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </main>
+    </nav>
 
-</div>
+    <!-- Navigation Mobile -->
+    <nav class="navbar-mobile">
+      <a class="brand js-target-scroll" href="/2/">
+        go<span class="text-primary">.</span>arch </a>
+
+
+      <!-- Navbar Collapse -->
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-mobile">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbar-mobile">
+        <div class="navbar-nav-mobile">
+
+          <ul class="menu">
+            <li class="item-101  deeper dropdown parent"><a class="dropdown-toggle" href="./homepage">Home <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-108"><a href="#">Home - Light</a></li>
+                <li class="item-109 active"><a href="./">Home - Dark</a></li>
+              </ul>
+            </li>
+            <li class="item-110"><a href="./about-us">About Us</a></li>
+            <li class="item-111 deeper dropdown parent"> <a class="btn-scroll" href="#">Projects <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-112"><a href="#">Projects - List</a></li>
+                <li class="item-113"><a href="#">Projects - Details</a></li>
+              </ul>
+            </li>
+            <li class="item-114 deeper dropdown parent"><a class="dropdown-toggle" href="./blog">Blog <i class="fa fa-angle-down"></i></a>
+              <ul>
+                <li class="item-115"><a href="#">Blog - List</a></li>
+                <li class="item-116"><a href="#">Blog - Post</a></li>
+              </ul>
+            </li>
+            <li class="item-117"><a href="./contacts">Contacts</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <div class="layout">
+
+    <!-- Start fullwidth-1 -->
+
+
+    <main class="main">
+
+
+      <!-- Start revolution slider -->
+
+      <div class="rev_slider_wrapper">
+        <div id="rev_slider" class="rev_slider fullscreenbanner">
+          <ul>
+            <!-- Slide 1 -->
+            <li data-transition="slotzoom-horizontal" data-slotamount="7" data-masterspeed="1000" data-fsmasterspeed="1000">
+
+              <!-- Main image-->
+
+              <img src="./images/slide1.jpg" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" class="rev-slidebg">
+
+
+
+
+
+
+          </ul>
+        </div>
+      </div>
+    </main>
+
+  </div>
 </div>
--- TranNhutTruong/2/2.php
+++ TranNhutTruong/2/2.php
@@ -24,48 +24,13 @@ file_put_contents("css/2.css", $result);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Module 2</title>
-    <link href="./css/contact-form-style.css" rel="stylesheet" />
-    <link href="./css/featherlight.min.css" rel="stylesheet" />
-    <link href="./css/spsimpleportfolio.css" rel="stylesheet" />
 
-    <script src="./js/jquery.min.js?1e174b993e3211aa87f92270aed62e25"></script>
-    <script src="./js/jquery-noconflict.js?1e174b993e3211aa87f92270aed62e25"></script>
-
-    <script src="./js/jquery.shuffle.modernizr.min.js"></script>
-    <script src="./js/featherlight.min.js"></script>
-    <script src="./js/spsimpleportfolio.js"></script>
 
 
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7cOswald:300,400,700" rel="stylesheet">
 
     <link rel="stylesheet" href="./css/2.css" />
 
-    <style>
-        #wrap-contact-form98 .sscf-popup,
-        #wrap-contact-form98 .rf_submit {
-            border-radius: 25px;
-        }
-
-        #wrap-contact-form98 .rf_submit {
-            background: #c5a47e;
-        }
-
-        #wrap-contact-form98 .rf_submit:hover {
-            background: #cc9f6b;
-        }
-
-        #wrap-contact-form98 .sscf-popup {
-            background: #c5a47e;
-        }
-
-        #wrap-contact-form98 .sscf-popup:hover {
-            background: #cc9f6b;
-        }
-
-        #wrap-contact-form98 .success {
-            color: #cc9f6b !important;
-        }
-    </style>
 
 </head>
 
